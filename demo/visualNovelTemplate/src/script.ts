afdd60358bc5b6289968c8ad7a794ea562ee076c
JaPNaA <japnaa@japnaa.dev>
2023-07-26 21:44:15 -0400

    Update visualNovelTemplate script

--- demo/visualNovelTemplate/src/script.ts
+++ demo/visualNovelTemplate/src/script.ts
@@ -1,4 +1,4 @@
-import { JaPNaAEngine2d, WorldElm } from "../../../build/JaPNaAEngine2d.js";
+import { JaPNaAEngine2d, SubscriptionsComponent, WorldElm, WorldElmWithComponents } from "../../../build/JaPNaAEngine2d.js";
 
 const GAME_TITLE = "Dokidoki Tokimeki Kirakira Raburabu Game";
 const WIDTH = 1280;
@@ -9,14 +9,30 @@ document.title = GAME_TITLE;
 const image = new Image();
 image.src = "tekitou-chan.png";
 
-class TitleScreen extends WorldElm {
+class TitleScreen extends WorldElmWithComponents {
+    private subscriptions: SubscriptionsComponent = this.addComponent(new SubscriptionsComponent());
+    
     constructor() {
         super();
         this.rect.width = WIDTH;
         this.rect.height = HEIGHT;
     }
 
+    _setEngine(engine: JaPNaAEngine2d) {
+        super._setEngine(engine);
+
+        this.subscriptions.subscribe(this.engine.mouse.onMousedown, this.continueHandler);
+        this.subscriptions.subscribe(this.engine.keyboard.getKeydownBus("Space"), this.continueHandler);
+    }
+
+    continueHandler() {
+        this.engine.world.addElm(new VisualNovelPlayer());
+        this.remove();
+    }
+
     drawRelative() {
+        super.drawRelative();
+
         const X = this.engine.canvas.X;
         const gradient = X.createLinearGradient(0, 0, 0, HEIGHT);
         gradient.addColorStop(0, "#ff8888");
@@ -35,6 +51,19 @@ class TitleScreen extends WorldElm {
         X.lineWidth = 4;
         X.strokeText(GAME_TITLE, textX, textY);
         X.fillText(GAME_TITLE, textX, textY);
+
+        const clickToContinueText = "Click or press space to start";
+        X.font = "16px Arial";
+        X.strokeText(clickToContinueText, textX, textY + 32);
+        X.fillText(clickToContinueText, textX, textY + 32);
+    }
+}
+
+class VisualNovelPlayer extends WorldElmWithComponents {
+    drawRelative() {
+        const X = this.engine.canvas.X;
+        X.fillStyle = "#f00";
+        X.fillRect(0, 0, 100, 100);
     }
 }
 
@@ -43,6 +72,6 @@ const engine = new JaPNaAEngine2d({
 });
 
 engine.world.addElm(new TitleScreen());
-image.addEventListener("load", () => {
-    engine.draw();
-});
+// image.addEventListener("load", () => {
+//     engine.draw();
+// });
